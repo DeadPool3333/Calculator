87c5a3531c6605e9513541f7525b709578784691
DeadPool <88374958+DeadPool3333@users.noreply.github.com>
2022-01-26 04:26:43 +0000

    Add evalute function

--- src/script.js
+++ src/script.js
@@ -1,3 +1,24 @@
-function func(result){
-    form.display_result.value = form.display_result.value+result;
-}
+function func(result) {
+    form.display_result.value = form.display_result.value + result;
+};
+
+function evalute(equation) {
+    const properties = Object.entries({
+        '+': '+',
+        '−': '-',
+        '×': '*',
+        '÷': '/'
+        }
+    )
+
+    for (let [key, value] of properties) {
+        var equation = equation.replace(key, value);
+    };
+
+    try {
+        form.display_result.value = eval(equation)
+    }
+    catch (error) {
+        form.display_result.value = 'Invalid expression'
+    };
+};
